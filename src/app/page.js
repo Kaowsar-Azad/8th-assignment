31900f872c1d8f462506f669e9186018824fb724
Pranto800 <kaowsarazad33@gmail.com>
2026-05-01 00:08:43 +0600

    add all photo page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/conponents/Banner";
+import TopGenerations from "@/conponents/TopGenerations";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
        <Banner></Banner>
+       <TopGenerations></TopGenerations>
     </div>
   );
 }
