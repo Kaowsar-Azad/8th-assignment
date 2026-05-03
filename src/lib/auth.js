845b9be2f554ff9ae39b1c2aba79e2a739c22e81
Pranto800 <kaowsarazad33@gmail.com>
2026-05-03 16:37:46 +0600

    signin page add

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,16 +2,17 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
-const db = client.db();
+const client = new MongoClient(process.env.MONGODB_URL);
+await client.connect();
+const db = client.db("pixgen");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
-  }),
-});kaowsar148_db_user
-Password
-UijB0ht0ramOyO1Z
-Hide
+    }),
+    emailAndPassword: { 
+    enabled: true, 
+  }
+});
 
