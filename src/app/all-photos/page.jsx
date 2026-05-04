ccc1fb938909aab7d7692825920ab64093e7ebf3
Pranto800 <kaowsarazad33@gmail.com>
2026-05-04 18:56:28 +0600

    update catagory section

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,20 +1,25 @@
+import Category from "@/conponents/Category";
 import PhotoCard from "@/conponents/PhotoCard";
 
 
 
-const AllPhotosPage = async () => {
-    
+const AllPhotosPage = async ({searchParams}) => {
+    const {category} = await searchParams;
     const res = await fetch('https://pixgen-project-x2ts.vercel.app/data.json')
     const photos = await res.json()
-
+    const FilterPhoto = category 
+  ? photos.filter(photo => 
+      photo.category.toLowerCase() == category.toLowerCase()
+    )
+  : photos
     
     return (
         <div>
 
             <h1 className="text-2xl font-bold m-4">All Photos</h1>
-
+            <Category></Category>
             <div className="grid grid-cols-4 gap-5">
-                {photos.map(photo=> <PhotoCard key={photo.id} photo={photo}/>)}
+                {FilterPhoto.map(photo=> <PhotoCard key={photo.id} photo={photo}/>)}
             </div>
 
             
